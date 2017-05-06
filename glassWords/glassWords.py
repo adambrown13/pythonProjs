6074fa61453e044320458f5ae46fef3badc10e83
Adam Brown <adambrown@berkeley.edu>
2017-05-05 19:00:37 -0700

    mostly finished. Bad character switching mechanism. Gonna refactor

--- glassWords/glassWords.py
+++ glassWords/glassWords.py
@@ -1,3 +1,8 @@
+"""
+Words that, if you wrote them on a piece of glass in a certain font, would look like an
+English word on the flip side
+"""
+
 import re
 
 file_name = 'wordsEn.txt'
@@ -7,7 +12,27 @@ with open(file_name) as f:
 total_words = [x.strip() for x in output]
 
 approved_letter = re.compile("^[^acefghjkrsyz]+$")
-trimmed_words = filter(approved_letter.match, total_words)
+flip_trimmed_words = map(lambda x: x[::-1], filter(approved_letter.match, total_words))  
 
-print(list(trimmed_words))
+final_words = []
 
+quickcheck = False
+
+for word in flip_trimmed_words:
+    new_word = ""
+    for c in word:
+        if c == "b":
+            new_word += "d"
+        elif c == "d":
+            new_word += "b"
+        elif c == "p":
+            new_word += "q"
+        elif c == "q":
+            new_word += "p"
+        else:
+            new_word += c
+    if new_word in total_words:
+        final_words.append(new_word)
+
+
+print (final_words)
